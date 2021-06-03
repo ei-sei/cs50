70fe09c7531da994418d075777380e68f79e53a9
HekateSK <itzshakeey@gmail.com>
2021-06-04 00:12:44 +0100

    github third  commit

--- hello.c
+++ hello.c
@@ -4,5 +4,9 @@ void main(){
     printf("Hello, World");
 }
 
-//commit test
-//Second github commit test
+/*
+Git status                                      -To check if any changes need to be uploaded
+Git add .                                       -To add all files to the repository
+Git commit -m "<enter message>"                 -Save the changes to the local repository
+
+*/
