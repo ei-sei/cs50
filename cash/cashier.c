f60f74135bc768c4340aaf7c363b23e620c31835
HekateSK <itzshakeey@gmail.com>
2021-06-06 14:03:33 +0100

    completed cash task

--- cash/cashier.c
+++ cash/cashier.c
@@ -1,28 +1,64 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <math.h>
 
-int getValue();
+/*
+TODO:
+-prompt user for an amount of change.
+-Convert dollars to cents 
+-user the largest coins possible, keeping track of coins used.
+-print the number of coins
+*/
 
-void main(){
+float getValue();
+int coins(int cents);
 
-    float change = getValue();
-    void getChange(int height);
+int main(void)
+{
+    float owed = getValue();
+    int cents = round(owed * 100);
+    printf("%d\n", coins(cents));
 }
 
 //Get initial value as input
-int getValue()
+float getValue(void)
 {
-    int cents;
+    float change;
     do
     {
-        printf("Enter amount: \n");
-        scanf("%d", &cents);
-    }
-    while (cents < 1);
-   
-    return cents;
+        printf("Change owed: \n");
+        scanf("%f", &change);
+    } while (change < 0.00);
+    return change;
 }
 
-//calculate the change
-void getChange(int change){
 
+//calculate the amount of coins
+int coins(int cents)
+{
+    int i = 0;
+    while (cents > 0)
+    {
+        if (cents >= 25)
+        {
+            cents -= 25;
+            i++;
+        }
+        else if (cents >= 10)
+        {
+            cents -= 10;
+            i++;
+        }
+        else if (cents >= 5)
+        {
+            cents -= 5;
+            i++;
+        }
+        else
+        {
+            cents -= 1;
+            i++;
+        }
+        
+    }
+    return i;
 }
